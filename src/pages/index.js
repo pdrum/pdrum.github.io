bde1c6240804e21228dc885946f94b96f56f246b
Pedram Hajesmaeeli <pedram.hajesmaeeli@glovoapp.com>
2022-08-27 18:57:55 +0200

    Install react bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import '../../static/bootstrap.min.css'
+import Alert from 'react-bootstrap/Alert';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+      <>
+        {[
+          'primary',
+          'secondary',
+          'success',
+          'danger',
+          'warning',
+          'info',
+          'light',
+          'dark',
+        ].map((variant) => (
+            <Alert key={variant} variant={variant}>
+              This is a {variant} alert—check it out!
+            </Alert>
+        ))}
+      </>
+  );
 }

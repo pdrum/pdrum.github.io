f6af40b1d97a733460705fcf7018a4560e00446a
Pedram Hajesmaeeli <pedram.hajesmaeeli@glovoapp.com>
2022-08-28 16:30:13 +0200

    Add query for hello world blog post

---
+++ src/md/hello-world.md
@@ -0,0 +1,8 @@
+---
+title: Hello World
+slug: /blog/hello-world
+excerpt: My first blog post
+date: 2022-08-28
+image: /avatar.jpg
+---
+Hello World
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import Layout from "../layout/Layout";
+import {graphql} from "gatsby";
 
 export default function medium() {
   return <Layout>
     Blog posts go here
   </Layout>
 }
+
+export const query = graphql`
+query AllMarkdownsQuery {
+  allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
+    nodes {
+      frontmatter {
+        date
+        image
+        slug
+        title
+        excerpt
+      }
+    }
+  }
+}`

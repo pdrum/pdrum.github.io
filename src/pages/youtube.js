05d388d0a2201c282621c8392cc7a7abee22d26d
Pedram Hajesmaeeli <pedram.hajesmaeeli@glovoapp.com>
2022-08-28 15:43:54 +0200

    Add contents to the youtube page

--- src/pages/youtube.js
+++ src/pages/youtube.js
@@ -3,6 +3,8 @@ import Layout from "../layout/Layout";
 
 export default function medium() {
   return <Layout>
-
+    <div className="alert alert-success" role="alert">
+      My Youtube videos will appear here.
+    </div>
   </Layout>
 }

0b35d12773f560e1b90497aa6d7c753d19e3b799
Pedram Hajesmaeeli <pedram.hajesmaeeli@glovoapp.com>
2022-10-23 02:36:02 +0200

    Remove width and height from iframes

--- src/pages/youtube.js
+++ src/pages/youtube.js
@@ -22,74 +22,74 @@ export default function medium() {
                     allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/DzT69G9CTNk"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/DzT69G9CTNk"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/Hbu6VmT790w"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/Hbu6VmT790w"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/aEMOABNjMnI"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/aEMOABNjMnI"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/37qY7vQlv8s"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/37qY7vQlv8s"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/jybLhn7t6Ls"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/jybLhn7t6Ls"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/37qY7vQlv8s"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/37qY7vQlv8s"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/fgT7x1W9JVM"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/fgT7x1W9JVM"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/XUfXyyqgT_M"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/XUfXyyqgT_M"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/T_8eoXonf0c"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/T_8eoXonf0c"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
               <Col lg={4}>
-                <iframe width="560" height="315"
-                        src="https://www.youtube.com/embed/mpJKpPtFQkc"
-                        title="YouTube video player" frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen/>
+                <iframe
+                    src="https://www.youtube.com/embed/mpJKpPtFQkc"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
               </Col>
             </Row>
           </Card.Body>

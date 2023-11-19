2fb592e6e84d860634191c7f570371b3f283aebe
Pedram Hajesmaeeli <pedram.esmaeeli@gmail.com>
2023-11-19 07:40:55 +0100

    Add video

--- src/pages/youtube.js
+++ src/pages/youtube.js
@@ -8,6 +8,23 @@ export default function medium() {
   return <Layout>
     <div className={`${styles.youtubeContainer} text-center bg-secondary`}>
       <Container>
+        <Card>
+
+          <Card.Header className="text-dark">
+            <Card.Title>Byte-Sized System Design</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Row>
+              <Col lg={4}>
+                <iframe
+                    src="https://www.youtube.com/embed/WjNdFldOoZA"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
         <Card className={`${styles.youtubePlaylist}`}>
           <Card.Header className="text-dark">
             <Card.Title>Object Oriented Design with Spring</Card.Title>
